6f705263886d1edb868180c01e619a93663f8f4c
alitu <alym11731996@gmail.com>
2024-03-09 01:14:20 +0300

    Priority in tesNG

---
+++ src/test/java/PriorityTestNG.java
@@ -0,0 +1,23 @@
+import org.testng.annotations.Test;
+
+public class PriorityTestNG {
+    @Test(priority = 1)
+    public void test1(){
+        System.out.println("test1");
+    }
+
+    @Test(priority = 3)
+    public void test2(){
+        System.out.println("test2");
+    }
+
+    @Test(priority = 4)
+    public void test3(){
+        System.out.println("test3");
+    }
+
+    @Test
+    public void test4(){
+        System.out.println("test4");
+    }
+}
