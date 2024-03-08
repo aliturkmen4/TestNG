0cc3f77e75cec6cdc2523c3d5cc631b44548436d
alitu <alym11731996@gmail.com>
2024-03-08 17:59:50 +0300

    Include & exclude methods

--- src/test/java/TestNG.java
+++ src/test/java/TestNG.java
@@ -22,4 +22,24 @@ public class TestNG {
         System.out.println("Bu bir basarisiz mail testidir!");
         driver.close();
     }
+
+    @Test
+    public void test1(){
+        System.out.println("test1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("test2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("test3");
+    }
+
+    @Test
+    public void test4(){
+        System.out.println("test4");
+    }
 }
